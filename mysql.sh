33f78996b1018598f74ffc8beb811507a1d6ab3c
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-01-15 20:46:37 +0530

    shell

--- mysql.sh
+++ mysql.sh
@@ -6,44 +6,42 @@ TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
-Userid=$(id -u)
-  echo "User id is: $Userid"
-
-  R="\e[31m"
-  G="\e[32m"
-  Y="\e[33m"
-  N="\e[0m"
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
 CHECK_ROOT(){
-    if [ $Userid -ne 0 ]
+    if [ $USERID -ne 0 ]
     then
-       echo -e "$R Please login with root previleges $N" | tee -a $LOG_FILE
-       exit 1
-    fi   
+        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        exit 1
+    fi
 }
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-       echo  -e "$2 is.. $R Failed $N" | tee -a $LOG_FILE
-       exit 1
+        echo -e "$2 is...$R FAILED $N"  | tee -a $LOG_FILE
+        exit 1
     else
-       echo -e "$2 is.. $G Success $N" | tee -a $LOG_FILE
-    fi       
+        echo -e "$2 is... $G SUCCESS $N" | tee -a $LOG_FILE
+    fi
 }
 
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
 CHECK_ROOT
 
-dnf install mysql-server -y &>>LOG_FILE
-VALIDATE $? "Installing MYSQL Server"
+dnf install mysql-server -y &>>$LOG_FILE
+VALIDATE $? "Installing MySQL Server"
 
-systemctl enable mysqld &>>LOG_FILE
-VALIDATE $? "Enabling MySql Server"
+systemctl enable mysqld &>>$LOG_FILE
+VALIDATE $? "Enabled MySQL Server"
 
-systemctl start mysqld &>>LOG_FILE
-VALIDATE $? "Start Mysql Server"
+systemctl start mysqld &>>$LOG_FILE
+VALIDATE $? "Started MySQL server"
 
 mysql_secure_installation --set-root-pass ExpenseApp@1 &>>LOG_FILE
 VALIDATE $? "Setting up root password"
